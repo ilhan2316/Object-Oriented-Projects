c58ba849e2e3d5acce7931ada4d0d1b8567c08e9
ilhan2316 <133736567+ilhan2316@users.noreply.github.com>
2023-08-28 01:07:49 -0500

    add in

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -1,1 +1,18 @@
 
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+       
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+
+        
+        System.out.println("Hello, " + name);
+
+        
+        scanner.close();
+    }
+}
