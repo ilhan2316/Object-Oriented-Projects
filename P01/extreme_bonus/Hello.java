4b5656aa7a8a447648c2ea715f811068edc0b5ab
ilhan2316 <133736567+ilhan2316@users.noreply.github.com>
2023-08-28 11:51:09 -0500

    Update Hello.java

--- P01/extreme_bonus/Hello.java
+++ P01/extreme_bonus/Hello.java
@@ -1,1 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        String username = System.getProperty("user.name");
+        
+        
+        System.out.println("Hello, " + username + "!");
+    }
+}
 
