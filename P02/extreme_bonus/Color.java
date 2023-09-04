26bba43d4b181860da0f2d6357ed474f948c309d
ilhan2316 <ilhgelle@gmail.com>
2023-09-03 20:54:54 -0500

    remove

--- P02/extreme_bonus/Color.java
+++ P02/extreme_bonus/Color.java
@@ -1,11 +1,11 @@
 public enum Color {
-    MAGENTA(0xFF00FF, "\u001B[35m"), // RGB for Magenta
-    ORANGE(0xFFA500, "\u001B[33m"), // RGB for Orange
-    PINK(0xFFC0CB, "\u001B[95m"), // RGB for Pink
-    CYAN(0x00FFFF, "\u001B[36m"); // RGB for Cyan
+    MAGENTA(0xFF00FF, "\u001B[35m"), 
+    ORANGE(0xFFA500, "\u001B[33m"), 
+    PINK(0xFFC0CB, "\u001B[95m"), 
+    CYAN(0x00FFFF, "\u001B[36m"); 
     
-    private final int rgb; // Red-Green-Blue integer
-    private final String ansiColorCode; // ANSI escape code for color
+    private final int rgb; 
+    private final String ansiColorCode; 
     
     private Color(int rgb, String ansiColorCode) {
         this.rgb = rgb;
