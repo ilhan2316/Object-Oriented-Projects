4515652ed5e656bd6c051b93ee4c380d737a3c20
ilhan2316 <ilhgelle@gmail.com>
2023-10-07 16:19:03 -0500

    final change- baseline for P06

--- P05/bonus/mdi/LibraryManager.java
+++ P05/bonus/mdi/LibraryManager.java
@@ -1,6 +1,5 @@
 package mdi;
 
-
 import java.util.List;
 import java.util.Scanner;
 import library.Library;
@@ -19,6 +18,7 @@ public class LibraryManager {
             Video video1 = createVideo("Ender's Game", "Gavin Hood", 2013, 114);
             Video video2 = createVideo("Remember the Titans", "Boaz Yakin", 2015, 120);
             Video video3 = createVideo("Shawshank Redemption", "Frank Darabont", 1994, 142);
+
             if (video1 != null) {
                 library.addPublication(video1);
             }
@@ -28,8 +28,8 @@ public class LibraryManager {
             if (video3 != null) {
                 library.addPublication(video3);
             }
+
             library.addPatron("Alice");
-            
 
             boolean exit = false;
 
@@ -54,31 +54,25 @@ public class LibraryManager {
 
                 System.out.print("\nEnter your choice: ");
                 int choice = scanner.nextInt();
-                scanner.nextLine(); 
+                scanner.nextLine();
 
                 switch (choice) {
                     case 1:
-                       
                         listPublications(library);
                         break;
                     case 2:
-                        
                         addPublication(scanner, library);
                         break;
                     case 3:
-                        
                         checkoutPublication(scanner, library);
                         break;
                     case 4:
-                        
                         checkinPublication(scanner, library);
                         break;
                     case 5:
-                        
                         listPatrons(library);
                         break;
                     case 6:
-                        
                         addPatron(scanner, library);
                         break;
                     case 0:
@@ -92,7 +86,7 @@ public class LibraryManager {
         }
     }
 
-    private static Video createVideo(String title, String director, int year, int runtime) {
+    public static Video createVideo(String title, String director, int year, int runtime) {
         try {
             return new Video(title, director, year, runtime);
         } catch (Video.InvalidRuntimeException e) {
@@ -101,89 +95,94 @@ public class LibraryManager {
         }
     }
 
-    private static void listPublications(Library library) {
+    public static void listPublications(Library library) {
         List<Publication> publications = library.getPublications();
         for (int i = 0; i < publications.size(); i++) {
             System.out.println(i + ": " + publications.get(i));
         }
     }
 
-    private static void addPublication(Scanner scanner, Library library) {
+    public static void addPublication(Scanner scanner, Library library) {
         System.out.print("Enter the title: ");
         String title = scanner.nextLine();
         System.out.print("Enter the author/director: ");
         String author = scanner.nextLine();
         System.out.print("Enter the copyright year: ");
         int copyright = scanner.nextInt();
-        scanner.nextLine(); 
+        scanner.nextLine();
 
         System.out.print("Is this a video? (yes/no): ");
         String isVideo = scanner.nextLine();
         if ("yes".equalsIgnoreCase(isVideo)) {
             System.out.print("Enter the runtime (in minutes): ");
             int runtime = scanner.nextInt();
-            scanner.nextLine(); 
-            Publication publication = new Video(title, author, copyright, runtime);
-            library.addPublication(publication);
+            scanner.nextLine();
+            try {
+                Publication publication = new Video(title, author, copyright, runtime);
+                library.addPublication(publication);
+                System.out.println("Video added successfully!");
+            } catch (Video.InvalidRuntimeException e) {
+                System.err.println("Invalid runtime exception: " + e.getMessage());
+            }
         } else {
             Publication publication = new Publication(title, author, copyright);
             library.addPublication(publication);
+            System.out.println("Publication added successfully!");
         }
-
-        System.out.println("Publication checked out successfully!");
-        System.out.println(library);
     }
 
-    private static void checkoutPublication(Scanner scanner, Library library) {
+    public static void checkoutPublication(Scanner scanner, Library library) {
         System.out.println("Select a publication to check out:");
         listPublications(library);
         System.out.print("Enter the index of the publication: ");
         int publicationIndex = scanner.nextInt();
-        scanner.nextLine(); 
+        scanner.nextLine();
 
-        if (isValidPublicationIndex(publicationIndex, library)) {
-            System.out.print("Enter the name of the patron: ");
-            String patron = scanner.nextLine();
-            library.checkout(publicationIndex, patron);
+        List<Publication> publications = library.getPublications();
+        if (isValidPublicationIndex(publicationIndex, publications)) {
+            System.out.print("Enter the patron's name: ");
+            String patronName = scanner.nextLine();
+            library.checkout(publicationIndex, patronName);
             System.out.println("Publication checked out successfully!");
         } else {
-            System.out.println("Invalid publication index!");
+            System.out.println("Invalid publication index. Please select a valid publication.");
         }
     }
 
-    private static void checkinPublication(Scanner scanner, Library library) {
+    public static void checkinPublication(Scanner scanner, Library library) {
         System.out.println("Select a publication to check in:");
         listPublications(library);
         System.out.print("Enter the index of the publication: ");
         int publicationIndex = scanner.nextInt();
-        scanner.nextLine(); 
+        scanner.nextLine();
 
-        if (isValidPublicationIndex(publicationIndex, library)) {
+        List<Publication> publications = library.getPublications();
+        if (isValidPublicationIndex(publicationIndex, publications)) {
             library.checkin(publicationIndex);
             System.out.println("Publication checked in successfully!");
         } else {
-            System.out.println("Invalid publication index!");
+            System.out.println("Invalid publication index. Please select a valid publication.");
         }
     }
 
-    private static void listPatrons(Library library) {
+    public static void listPatrons(Library library) {
         List<String> patrons = library.getPatrons();
         for (int i = 0; i < patrons.size(); i++) {
             System.out.println(i + ": " + patrons.get(i));
         }
     }
-    
-    private static void addPatron(Scanner scanner, Library library) {
 
+    public static void addPatron(Scanner scanner, Library library) {
         System.out.print("Enter the name of the new patron: ");
         String newPatron = scanner.nextLine();
         library.addPatron(newPatron);
         System.out.println("New patron added successfully!");
     }
-    
-    private static boolean isValidPublicationIndex(int index, Library library) {
-        return index >= 0 && index < library.getPublications().size();
+
+    private static boolean isValidPublicationIndex(int index, List<Publication> publications) {
+        return index >= 0 && index < publications.size();
     }
 }
+
 
 
